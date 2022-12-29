e559c2e94b02e43cb12f6f8be710c04dbe39c841
lunarhalocloud <taekgeun9025@gmail.com>
2022-12-29 09:27:54 +0900

    updated

--- server_express.js
+++ server_express.js
@@ -1,51 +1,43 @@
-const e = require("express");
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 
+app.use(express.json());
+
+app.use(bodyParser.json());
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessSatus: 200,
+}));
+
+app.get("/", (request, response) =>
+{
+  response.send('This is backend page');
+});
+
 /* localhost:3000/ 접속시 나올 메시지 */
-app.get("/", (request, response) => {
-  var all_data = request.query.all;
-  var platform = request.query.platform;
-  if (all_data == 1)
+app.post("/", (request, response) => {
+  var platform = request.body.platform;
+  var search = request.body.search;
+  MongoClient.connect('mongodb+srv://cse20181660:rlrkdldktm@cluster0.xzos63t.mongodb.net/?retryWrites=true&w=majority', function(err,db)
   {
-    MongoClient.connect('mongodb+srv://cse20181660:rlrkdldktm@cluster0.xzos63t.mongodb.net/?retryWrites=true&w=majority', function(err,db)
-    {
-         if (err) throw err;
-         dbo = db.db(platform);
-         dbo.collection('game').find({}).toArray(function(err, game) {
-            if (err) return response.status(500).json({error: err});
-            if (!game) return response.status(404).json({error: 'game not found'});
-            response.json(game);
-            db.close();
-         });
+    if (err) throw err;
+    dbo = db.db(platform);
+    const query = {title : {$regex: search }};
+    dbo.collection('game').find(query).toArray(function(err, game) {
+      if (err)return response.status(500).json({error: err});
+      if (!game) return response.status(404).json({error: 'game not found'});
+      console.log(game);
+      response.json(game);
+      db.close();
     });
-  }
-  else
-  {
-    var search = request.query.search;
-    MongoClient.connect('mongodb+srv://cse20181660:rlrkdldktm@cluster0.xzos63t.mongodb.net/?retryWrites=true&w=majority', function(err,db)
-    {
-        if (err) throw err;
-        dbo = db.db(platform);
-        const query = "/" + search + "/";
-        dbo.collection('game').find({title: query}).toArray(function(err, game) {
-            if (err)return response.status(500).json({error: err});
-            if (!game) return response.status(404).json({error: 'game not found'});
-            response.json(game);
-            db.close();
-        });
-    });
-  }
+  });
 });
 
-/* localhost:3000/ 혹은 localhost:3000/main 외의
-get하지 않은 페이지 접속시 나올 메시지. */
-app.use((request, response) => {
-  response.send(`<h1>Sorry, page not found (msg by Express Server !! ):(</h1>`);
-});
-
-/* 3000포트에서 서버 구동 */
 app.listen(4000, () => {
   console.log("localhost:4000 에서 서버가 시작됩니다.");
 });
--- temp/src/Inputquery.js
+++ temp/src/Inputquery.js
@@ -4,56 +4,63 @@ import axios from 'axios';
  
 export default function Inputquery() {
   const [text, setText] = useState("");
+  const [out1, setTable1] = useState("");
+  const [out2, setTable2] = useState("");
   const onChange = (e) => {
     setText(e.target.value);
   };
-  const onClick1 = () => {
-    axios.get('http://localhost:3000', {
-      params:
-      {
-        all_data : 0,
+  const onClick1 = (e) => {
+    fetch('http://localhost:4000', {
+      method: "post",
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify({
         platform : "metacritic",
         search: text
-      }
-    }).then(function(response){
-      const obj = JSON.parse(response);
-      var out = "<table><tr><th>Rank</th><th>Title</th><th>Platform</th><th>Released</th><th>Score</th></tr>";
-      const temp = obj.map(function(item) {
+      })
+    }).then(response => response.json())
+    .then(response => {
+      var out = "<table><thead><tr><th>Rank</th><th>Title</th><th>Platform</th><th>Released</th><th>Score</th></tr></thead></tbody>";
+      const temp = response.map(function(item) {
         out += "<tr><td>" + item.rank + "</td>";
         out += "<td>" + item.title + "</td>";
         out += "<td>" + item.platform + "</td>";
         out += "<td>" + item.released + "</td>";
         out += "<td>" + item.score + "</td></tr>";
       });
-      out += "</table>";
-      return out;
+      out += "</tbody></table>";
+      setTable1(out);
     });
   };
-    const onClick2 = () => {
-      axios.get('http://localhost:4000', {
-        params:
-        {
-          all_data : 0,
+    const onClick2 = (e) => {
+      fetch('http://localhost:4000', {
+        method: "post",
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
           platform : "opencritic",
           search: text
-        }
-      }).then(function(response){
-        const obj = JSON.parse(response);
-        var out = "<table><tr><th>Rank</th><th>Title</th><th>Platform</th><th>Released</th><th>Score</th></tr>";
-        const temp = obj.map(function(item) {
+        })
+      }).then(response => response.json())
+      .then(response => {
+        var out = "<table><thead><tr><th>Rank</th><th>Title</th><th>Platform</th><th>Released</th><th>Score</th></tr></thead></tbody>";
+        const temp = response.map(function(item) {
           out += "<tr><td>" + item.rank + "</td>";
           out += "<td>" + item.title + "</td>";
           out += "<td>" + item.platform + "</td>";
           out += "<td>" + item.released + "</td>";
           out += "<td>" + item.score + "</td></tr>";
         });
-        out += "</table>";
-        return out;
+        out += "</tbody></table>";
+        setTable2(out);
       });
   };
   
   return (
-    <div className="head">
+    <div>
+      <div className="head">
       Search<br></br>
       <input onChange={onChange} value={text} />
       <button onClick={onClick1}>Metacritic Search</button>
@@ -63,9 +70,12 @@ export default function Inputquery() {
       </div>
       <div className="left" id="left">
         Metacritic Search
+        <div className="score_table" dangerouslySetInnerHTML={{__html: out1}}></div>
       </div>
       <div className="right" id="right">
         Opencritic Search
+        <div className="score_table" dangerouslySetInnerHTML={{__html: out2}}></div>
+      </div>
       </div>
     </div>
   );
